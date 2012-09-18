abff375bf84e949fa8b4350780a6d3213770e790
Alec Embke <aembke@uoregon.edu>
2012-09-18 14:38:00 -0700

    Cleaned up typos and old comments in our-staff.php

--- our-staff.php
+++ our-staff.php
@@ -1,48 +1,49 @@
 <?php
 /*
 * The page template for displaying the staff photos in a grid format.
-* 	Uses bootstrap css classes.
-* 
 */
 ?>
 
-<!-- main content wrapper -->
 <?php
-// preprocessing
+// pre-processing
 $images = scandir('/img/staff-photos/*.jpg');
 $index = 0;
 $settings = array(
 	'dir_prefix' => '/img/staff-photos/',
 	'rows' => 7,
-	'columns' => 6,m
+	'columns' => 6,
 );
 /* column number must be an integer divisor of 12 */
 ?>
 
 <?php get_header(); ?>
-	<div class="container" >
-	<?php
-	// this is how I'd prefer to do this
-	foreach($images as $image){
-		if($image === "." || $image === ".."){
-			continue;
-		}
-		if($index % $settings['columns'] == 0){
-			// new row
-			echo "<div class='row' > ";
-		}
-	 
-		echo "<div class='span". $settings['columns'] / 12 ."' >";
-		echo "<p><img src='". $settings['dir_prefix'] . $image ."' title='". $image ."' /></p>";
-		echo "</div>";
-		
-		if(($index != 0 && $index % $settings['columns'] == 0) || $index == count($images) - 1){
-			//end row
-			echo "</div>";
-		}
-		$index++;
-	}
-	?>
+	<div id="primary" class="site-content" >
+		<div id="content" >
+			<div class="container" >
+			<?php
+				foreach($images as $image){
+				if($image === "." || $image === ".." || $image{0} == '.'){ 
+					// skip if image is a directory reference or hidden
+					continue;
+				}
+				if($index % $settings['columns'] == 0){
+					// new row
+					echo "<div class='row' > ";
+				}
+			 
+				echo "<div class='span". $settings['columns'] / 12 ."' >";
+				echo "<p><img src='". $settings['dir_prefix'] . $image ."' title='". $image ."' /></p>";
+				echo "</div>";
+				
+				if(($index != 0 && $index % $settings['columns'] == 0) || $index == count($images) - 1){
+					//end row
+					echo "</div>";
+				}
+				$index++;
+			}
+			?>
+			</div>
+		</div>
 	</div>
 <?php get_footer(); ?>
 
