24a0a06ca78aa3af6992f6a4e2dbb9b3fce56c7d
Ivar Vong <ivong@dailyemerald.com>
2012-09-28 23:50:56 +0000

    use the post time, not the modified time, on the single post view

--- content.php
+++ content.php
@@ -48,7 +48,7 @@
 					</a>
 				    
 					<?php //echo 'Published <time class="timeago" datetime="'.get_the_modified_time("c").'">'.get_the_modified_time("l, M. j \a\\t g:i a").'</time>.'; ?>
-				    <?php echo ' on '.get_the_modified_time("l, M. j \a\\t g:i a").'.'; ?>
+				    <?php echo ' on '.get_the_time("l, M. j \a\\t g:i a").'.'; ?>
 				
 					<div class="facebook-like-wrapper pull-right">
 						<fb:like href="<?php the_permalink(); ?>" send="true" layout="button_count" width="100" show_faces="false"></fb:like>
