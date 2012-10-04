85a6d5b8e3ca898a0a576d8e5be9a86a6b2e62ca
Alec Embke <aembke@uoregon.edu>
2012-10-03 17:49:49 -0700

    fixed popover hover hiding issue

--- js/our-staff.js
+++ js/our-staff.js
@@ -2,11 +2,39 @@
 (function($){
 
     $(document).ready(function(){
+			  /*
+			   * This is incredibly hackish. Ivar, I wouldn't worry about wrapping your head around this.
+			   */
 			  $(".hover").popover({
-					html: true,
-				        placement: 'bottom',
-					trigger: 'hover'
-			  });
+                                        placement: 'bottom',
+                                        trigger: 'manual'
+                          });
+
+
+			  $(".hover").mouseenter(function(){
+                              $curr = $(this);
+			      $curr.css('cursor', 'pointer');
+                           
+			      $('.hover').popover('destroy');
+			      $(".hover").popover({
+                                        placement: 'bottom',
+                                        trigger: 'manual'
+                              });
+			      $curr.popover('show');
+		           });
+
+			   $(document).click(function(){
+                               $('.hover').popover('destroy');
+			       $(".hover").popover({
+                                        placement: 'bottom',
+                                        trigger: 'manual'
+                               });
+			    });
+
+		        $(".hover").mouseleave(function(){
+                              $(this).css('cursor', 'auto'); 
+                        });
+
 	    
 	});
     
