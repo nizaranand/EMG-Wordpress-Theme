2f4a27116eab8473c3dfe7806d1b4267e0907124
Alec Embke <aembke@uoregon.edu>
2012-10-18 23:29:11 -0700

    reordered dependencies

--- app-page.php
+++ app-page.php
@@ -16,14 +16,13 @@
 	wp_enqueue_script("underscore", get_template_directory_uri() . "/js/app-view/libs/underscore/underscore.min.js", true);
 	wp_enqueue_script("backbone", get_template_directory_uri() . "/js/app-view/libs/backbone/backbone.min.js", true);
 	wp_enqueue_script("backbone-state", get_template_directory_uri() . "/js/app-view/libs/state-manager/backbone-statemanager.js", true);
-	// add backbone objects
-	wp_enqueue_script("app-model", get_template_directory_uri() . "/js/app-view/models/App.js", true);
-	wp_enqueue_script("app-view", get_template_directory_uri() . "/js/app-view/views/AppView.js", true);
-	wp_enqueue_script("storywidgets-collection", get_template_directory_uri() . "/js/app-view/collections/StoryWidgets.js", true);
-	wp_enqueue_script("storywidgets-view", get_template_directory_uri() . "/js/app-view/views/StoryWidgetsView.js", true);	
+	// add backbone objects	
 	wp_enqueue_script("storywidget-model", get_template_directory_uri() . "/js/app-view/models/StoryWidget.js", true);
 	wp_enqueue_script("storywidget-view", get_template_directory_uri() . "/js/app-view/views/StoryWidgetView.js", true);
-	wp_enqueue_script("stories-router", get_template_directory_uri() . "/js/app-view/routers/StoriesRouter.js", true);
+	wp_enqueue_script("storywidgets-collection", get_template_directory_uri() . "/js/app-view/collections/StoryWidgets.js", true);
+	wp_enqueue_script("storywidgets-view", get_template_directory_uri() . "/js/app-view/views/StoryWidgetsView.js", true);
+	wp_enqueue_script("app-model", get_template_directory_uri() . "/js/app-view/models/App.js", true);
+	wp_enqueue_script("app-view", get_template_directory_uri() . "/js/app-view/views/AppView.js", true);
 	//wp_enqueue_script("stories-router", get_template_directory_uri() . "/js/app-view/StoriesRouter.js", true);
 	wp_enqueue_script("app-helpers", get_template_directory_uri() . "/js/app-view/app-helpers.js", true);
 	wp_enqueue_script("app-init", get_template_directory_uri() . "/js/app-view/app-init.js", true);
