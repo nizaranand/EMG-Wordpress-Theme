c0ce4cc695ce466a57ac2fece22462f1d547827c
Alec Embke <aembke@uoregon.edu>
2012-10-18 22:57:19 -0700

    stop adding markup when you fail to merge git, it's really annoying

--- app-page.php
+++ app-page.php
@@ -23,11 +23,8 @@
 	wp_enqueue_script("storywidgets-view", get_template_directory_uri() . "/js/app-view/views/StoryWidgetsView.js", true);	
 	wp_enqueue_script("storywidget-model", get_template_directory_uri() . "/js/app-view/models/StoryWidget.js", true);
 	wp_enqueue_script("storywidget-view", get_template_directory_uri() . "/js/app-view/views/StoryWidgetView.js", true);
-<<<<<<< HEAD
 	wp_enqueue_script("stories-router", get_template_directory_uri() . "/js/app-view/routers/StoriesRouter.js", true);
-=======
 	//wp_enqueue_script("stories-router", get_template_directory_uri() . "/js/app-view/StoriesRouter.js", true);
->>>>>>> dcbf3266ca4bd8742538dc54fcf53cc73957639a
 	wp_enqueue_script("app-helpers", get_template_directory_uri() . "/js/app-view/app-helpers.js", true);
 	wp_enqueue_script("app-init", get_template_directory_uri() . "/js/app-view/app-init.js", true);
 	
