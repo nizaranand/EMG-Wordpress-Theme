65d6556fbf9fa2426772c37274bcc2c89c695b5b
Alec Embke <aembke@uoregon.edu>
2012-10-30 19:35:54 -0700

    finished rewriting scrollbar

--- js/app-view/views/StoryWidgetsView.js
+++ js/app-view/views/StoryWidgetsView.js
@@ -1,103 +1,93 @@
+var app = app || {}; 
 
-var app = app || {};
+( function($, _, Backbone) {
 
-(function($, _, Backbone){	
-	
-app.StoryWidgetsView = Backbone.View.extend({
-	// $arrow: $("<div id='arrow' ><img src='blah' ></div>"),
-	el: "#widgets-list",
-	timer: false,
-	template_options: {},
-	/*scrollbar_opts: {
-		set_height: "100%",
-		set_width: "100%",
-		scrollInertia: 400,
-		scrollEasing: "easeOutCirc",
-		autoDraggerLength: true,
-		scrollButtons: {
-			enable: true
-		},
-		advanced:{
-    		updateOnBrowserResize:true, 
-    		updateOnContentResize:true, 
-  		},
-  	    callbacks:{ 
-    		onTotalScroll: this.hit_bottom, 
-    		onTotalScrollOffset: 50 
-  		}
-	},*/
-	
-	initialize: function(){
-		app.showStory = this.showStory;
-		this.collection.on('add', this.addOne, this);
-		this.collection.on('reset', this.addAll, this);
-		this.timer = setInterval(this.refresh, 60 * 1000);
-		$("#story-widgets").mCustomScrollbar({
-			set_height: "100%",                                                                                                                                                                
-            set_width: "100%",                                                                                                                                                                 
-            scrollInertia: 400,                                                                                                                                                                
-            scrollEasing: "easeOutCirc",
-            autoDraggerLength: true,
-            scrollButtons:{
-                enable: true 
-            },
-            advanced:{                                                                                                                                                                         
-                updateOnBrowserResize:true,
-                updateOnContentResize:true,
-            }
+		app.StoryWidgetsView = Backbone.View.extend({
+			// $arrow: $("<div id='arrow' ><img src='blah' ></div>"),
+			el : "#widgets-list",
+			timer : false,
+			template_options : {},
 
+			initialize : function() {
+				app.showStory = this.showStory;
+				this.collection.on('add', this.addOne, this);
+				this.collection.on('reset', this.addAll, this);
+				this.timer = setInterval(this.refresh, 60 * 1000);
+				$("#story-widgets").mCustomScrollbar({
+					set_width : false, /*optional element width: boolean, pixels, percentage*/
+					set_height : false, /*optional element height: boolean, pixels, percentage*/
+					horizontalScroll : false, /*scroll horizontally: boolean*/
+					scrollInertia : 550, /*scrolling inertia: integer (milliseconds)*/
+					scrollEasing : "easeOutCirc", /*scrolling easing: string*/
+					mouseWheel : "auto", /*mousewheel support and velocity: boolean, "auto", integer*/
+					autoDraggerLength : true, /*auto-adjust scrollbar dragger length: boolean*/
+					scrollButtons : {/*scroll buttons*/
+						enable : true, /*scroll buttons support: boolean*/
+						scrollType : "continuous", /*scroll buttons scrolling type: "continuous", "pixels"*/
+						scrollSpeed : 20, /*scroll buttons continuous scrolling speed: integer*/
+						scrollAmount : 40 /*scroll buttons pixels scroll amount: integer (pixels)*/
+					},
+					advanced : {
+						updateOnBrowserResize : true, /*update scrollbars on browser resize (for layouts based on percentages): boolean*/
+						updateOnContentResize : true, /*auto-update scrollbars on content resize (for dynamic content): boolean*/
+						autoExpandHorizontalScroll : false /*auto expand width for horizontal scrolling: boolean*/
+					}
+					// add callbacks later
+				});
+			},
 
-        });
-	},
-	
-	render: function(){
-		//	rendering is done on the individual elements in the collection
-	},
-	
-	addAll: function(){
-		$("#widgets-list").html("");
-		app.widgets.each(this.addOne, this);
-		$("li.story-widget").click(function(){
-		    app.showStory($(this).attr("id"));				
-        });
-	},
-	
-	addOne: function(widget){
-		widget.set({ parent: this });
-		var widgetView = new app.StoryWidgetView({ model: widget });
-		widget.set({ view: widgetView });
-		widget.get("view").render();
-		$("#widgets-list").append(widget.get("view").$el);
-	},
-	
-	scrolled: function(){
-		
-	},
-	
-	hit_bottom: function(){
-		//app.widgets.fetch();
-	},
-	
-	refresh: function(){
-		app.widgets.fetchWithCallbacks();
-	},
-												
-    showStory: function(id){	
-        var model = app.widgets.get(id);
-        var $content_template = $("#storycontent-template");
-        var content_opts = {
-            story_title: model.get("title"),
-            story_content: model.get("content"),
-            story_author: model.get("author")
-        };
-        var content = _.template($content_template.html(), content_opts);
-		var $content = $("#story-content");
-		$content.fadeOut(150, function(){
-			$content.html(content);
-			$content.fadeIn(150);
+			render : function() {
+				//	rendering is done on the individual elements in the collection
+			},
+
+			addAll : function() {
+				$("#widgets-list").html("");
+				app.widgets.each(this.addOne, this);
+				$("li.story-widget").click(function() {
+					app.showStory($(this).attr("id"));
+				});
+			},
+
+			addOne : function(widget) {
+				widget.set({
+					parent : this
+				});
+				var widgetView = new app.StoryWidgetView({
+					model : widget
+				});
+				widget.set({
+					view : widgetView
+				});
+				widget.get("view").render();
+				$("#widgets-list").append(widget.get("view").$el);
+			},
+
+			scrolled : function() {
+
+			},
+
+			hit_bottom : function() {
+				//app.widgets.fetch();
+			},
+
+			refresh : function() {
+				app.widgets.fetchWithCallbacks();
+			},
+
+			showStory : function(id) {
+				var model = app.widgets.get(id);
+				var $content_template = $("#storycontent-template");
+				var content_opts = {
+					story_title : model.get("title"),
+					story_content : model.get("content"),
+					story_author : model.get("author")
+				};
+				var content = _.template($content_template.html(), content_opts);
+				var $content = $("#story-content");
+				$content.fadeOut(150, function() {
+					$content.html(content);
+					$content.fadeIn(150);
+				});
+			}
 		});
-	}
-
-
-});
-}(jQuery, _, Backbone));
+	}(jQuery, _, Backbone));
