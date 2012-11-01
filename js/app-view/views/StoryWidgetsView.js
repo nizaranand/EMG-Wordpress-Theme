a938b8e419a6dd610da7325af0a9bf3d79bc49ae
Alec Embke <aembke@uoregon.edu>
2012-10-31 23:47:06 -0700

    switched spinner context so it works with the collection

--- js/app-view/views/StoryWidgetsView.js
+++ js/app-view/views/StoryWidgetsView.js
@@ -17,6 +17,9 @@ var app = app || {}; ( function($, _, Backbone) {
 				app.sliding = false;
 				app.arrowDestination = 0;
 				app.animateTimer = false;
+				app.startSpinner = this.startSpinner;
+				app.stopSpinner = this.stopSpinner;
+				
 				this.collection.on('add', this.addOne, this);
 				this.collection.on('reset', this.addAll, this);
 				$(window).resize(app.resizeApp);
@@ -180,7 +183,37 @@ var app = app || {}; ( function($, _, Backbone) {
 				});
 			},
 
-			
+			startSpinner : function() {
+				var opts = {
+					lines : 13, // The number of lines to draw
+					length : 7, // The length of each line
+					width : 4, // The line thickness
+					radius : 10, // The radius of the inner circle
+					corners : 1, // Corner roundness (0..1)
+					rotate : 0, // The rotation offset
+					color : '#333', // #rgb or #rrggbb
+					speed : 2, // Rounds per second
+					trail : 60, // Afterglow percentage
+					shadow : false, // Whether to render a shadow
+					hwaccel : false, // Whether to use hardware acceleration
+					className : 'spinner', // The CSS class to assign to the spinner
+					zIndex : 2e9, // The z-index (defaults to 2000000000)
+					top : 'auto', // Top position relative to parent in px
+					left : 'auto' // Left position relative to parent in px
+				};
+				this.spinner = new Spinner(opts);
+				$("#app-view").animate({
+					opacity : "0.5"
+				}, 200);
+				this.spinner.spin($("#app-view"));
+			},
+
+			stopSpinner : function() {
+				$("#app-view").animate({
+					opacity : "1.0"
+				}, 500);
+				this.spinner.stop();
+			}
 			
 			
 		});
