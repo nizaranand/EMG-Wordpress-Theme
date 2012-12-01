33a9092482c5284c89e88427bf56103436fd6b35
Ivar Vong <ivong@dailyemerald.com>
2012-12-01 14:56:56 -0800

    removed 2 third party js trackers, add my heroku test one

--- footer.php
+++ footer.php
@@ -26,37 +26,7 @@
 
 <?php wp_footer(); ?>
 
-<script type="text/javascript">
-  var _gauges = _gauges || [];
-  (function() {
-    var t   = document.createElement('script');
-    t.type  = 'text/javascript';
-    t.async = true;
-    t.id    = 'gauges-tracker';
-    t.setAttribute('data-site-id', '506f45a5f5a1f5704e00002a');
-    t.src = '//secure.gaug.es/track.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(t, s);
-  })();
-</script>
-
-<script type="text/javascript">
-  var GoSquared = {};
-  GoSquared.acct = "GSN-135048-E";
-  (function(w){
-    function gs(){
-      w._gstc_lt = +new Date;
-      var d = document, g = d.createElement("script");
-      g.type = "text/javascript";
-      g.src = "//d1l6p2sc9645hc.cloudfront.net/tracker.js";
-      var s = d.getElementsByTagName("script")[0];
-      s.parentNode.insertBefore(g, s);
-    }
-    w.addEventListener ?
-      w.addEventListener("load", gs, false) :
-      w.attachEvent("onload", gs);
-  })(window);
-</script>
+<script src="http://intense-thicket-1581.herokuapp.com/track.js"></script>
 
 <script src="http://www.goducks.com/mediaPortal/scripts/inline.js"></script>
 
